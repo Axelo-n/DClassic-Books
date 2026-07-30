d410a743da786900a638bfa4f72d55b6dfdb1660
joshmoritty <josh.moritty@gmail.com>
2026-07-30 11:28:21 +0700

    fix: login input

--- app/src/main/java/com/example/dclassicbooks/LoginActivity.java
+++ app/src/main/java/com/example/dclassicbooks/LoginActivity.java
@@ -29,6 +29,7 @@ public class LoginActivity extends AppCompatActivity {
         Window window = getWindow();
         window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
         window.setStatusBarColor(ContextCompat.getColor(this, R.color.surface));
+
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
             window.getInsetsController().setSystemBarsAppearance(
                     WindowInsetsController.APPEARANCE_LIGHT_STATUS_BARS,
